78574dbe0061f2daab306630fd8b630e86770457
monir elbaz <mazir210@gmail.com>
2020-06-04 21:24:23 +0200

    first commit

---
+++ src/build/precache-manifest.dfb66f37b73cc863827e350f837e3eda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a4325e43f2b8e52ae9dfbb66d35967e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "277b1724b7b3774cb067",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "a1850b1e29f83404fcfa",
+    "url": "/static/js/2.11aa829e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.11aa829e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "277b1724b7b3774cb067",
+    "url": "/static/js/main.af347e34.chunk.js"
+  },
+  {
+    "revision": "36adbc7179ce49d2ac8a",
+    "url": "/static/js/runtime-main.79e3e27f.js"
+  }
+]);
